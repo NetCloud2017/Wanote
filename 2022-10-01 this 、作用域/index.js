6cf817db4c3c912153c48c927263a96d7554eb11
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-11-04 00:52:25 +0800

    手写 call

--- 2022-10-01 this 、作用域/index.js
+++ 2022-10-01 this 、作用域/index.js
@@ -1,7 +1,30 @@
 // 手写bind 函数
 Function.prototype.abind = function (context, ...rest) {
-  const fn = this;
-  return function (...agrs) {
-    return fn.apply(context, [...rest, ...agrs]);
-  };
+	const fn = this;
+	console.log(this);
+	return function (...agrs) {
+		return fn.apply(context, [...rest, ...agrs]);
+	};
 };
+
+// test
+
+let a = {
+	name: "joker",
+};
+
+function logName() {
+	console.log(this.name);
+}
+logName.abind(a)();
+
+// 手写 apply 或 call
+
+Function.prototype.called = called;
+function called(context, ...rest) {
+	context.fn = this;
+	if (context) {
+		let result = context.fn(...rest);
+		return result;
+	}
+}

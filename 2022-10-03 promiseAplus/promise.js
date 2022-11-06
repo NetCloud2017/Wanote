b7fef71397abbd0d572a5fb167052c2079ee8e0c
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-11-06 23:55:07 +0800

    promise  A+ 规范

--- 2022-10-03 promiseAplus/promise.js
+++ 2022-10-03 promiseAplus/promise.js
@@ -1,9 +1,10 @@
+// 手写 promise
 class ClonePromise {
-  constructor() {
-    this.state = "pendding";
-  }
+	constructor() {
+		this.state = "pendding";
+	}
 
-  then(func) {}
-  resolve() {}
-  reject() {}
+	then(func) {}
+	resolve() {}
+	reject() {}
 }
